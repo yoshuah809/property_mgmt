b28f80583cbde3330275aedccf322d3bdc41bc65
Josue Garcia <josue.garcia809@outlook.com>
2022-06-27 08:23:19 -0500

    created property model

--- property_app/admin.py
+++ property_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from property_app.models import Property
+
 # Register your models here.
+
+admin.site.register(Property)
--- property_app/models.py
+++ property_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Property(models.Model):
+    address = models.CharField(max_length=250)
+    country = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    images = models.CharField(max_length=900)
+    active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.address
+    
+    
