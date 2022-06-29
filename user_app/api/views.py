4d7c439f415e900d32965cee0b21715d8c6977a0
Josue Garcia <josue.garcia809@outlook.com>
2022-06-29 14:06:13 -0500

    created logout view

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -7,6 +7,13 @@ from rest_framework.authtoken.models import Token
 
 # Create your views here.
 @api_view(['POST',])
+def logout_view(request):
+    if request.method == 'POST':
+       request.user.auth_token.delete()
+       return Response(status=status.HTTP_200_OK)
+   
+
+@api_view(['POST',])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
