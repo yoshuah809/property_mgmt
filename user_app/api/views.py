c54e006cd59e45b49a82bcfd3abca8af4393498b
Josue Garcia <josue.garcia809@outlook.com>
2022-06-30 13:27:51 -0500

    implemented full JWT Token authorization

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,8 +2,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from user_app.api.serializers import RegistrationSerializer
-from user_app import models
+#from user_app import models
 from rest_framework.authtoken.models import Token
+from rest_framework_simplejwt.tokens  import RefreshToken
 
 # Create your views here.
 @api_view(['POST',])
@@ -24,10 +25,14 @@ def registration_view(request):
             data['response']='User was successfully registered'
             data['username'] = account.username
             data['email'] = account.email
-            token = Token.objects.get(user=account).key
-            data['token']= token
-            #return Response(serializer.data, status=status.HTTP_201_CREATED)
+            # token = Token.objects.get(user=account).key
+            # data['token']= token
+            refresh = RefreshToken.for_user(account)
+            data['token']={
+                'refresh', str(refresh),
+                'access', str(refresh.access_token)
+            }
+            
         else:
             data=serializer.errors
-        
         return Response(data, status=status.HTTP_201_CREATED)    
